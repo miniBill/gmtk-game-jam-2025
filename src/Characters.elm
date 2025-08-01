0c03157c100f0650ca8993ab7e12b4126ae2277d
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2025-08-01 13:48:13 +0200

    Interactive character editor

--- src/Characters.elm
+++ src/Characters.elm
@@ -1,13 +1,29 @@
 module Characters exposing (main)
 
 import Avataaars
+import Avataaars.Clothes exposing (Clothes)
+import Avataaars.Face exposing (Face)
+import Avataaars.FacialHair as FacialHair
 import Avataaars.Graphics exposing (Graphics(..))
+import Avataaars.SkinTone exposing (SkinTone)
+import Avataaars.Top as Top exposing (Top(..))
 import Avatars
+import Browser
+import List.Extra
 import TypedSvg exposing (g, svg)
-import TypedSvg.Attributes exposing (transform, viewBox)
+import TypedSvg.Attributes exposing (cursor, transform, viewBox)
 import TypedSvg.Core exposing (Svg)
-import TypedSvg.Types exposing (Transform(..))
+import TypedSvg.Events exposing (onClick)
+import TypedSvg.Types exposing (Cursor(..), Transform(..))
 import Types exposing (Character(..))
+
+
+type alias Model =
+    Maybe ( Character, Graphics )
+
+
+type alias Msg =
+    Model
 
 
 allCharacters : List ( Character, Graphics )
@@ -25,19 +41,91 @@ allCharacters =
     go ( June, Bat ) []
 
 
-main : Svg msg
+main : Program () Model Msg
 main =
-    allCharacters
+    Browser.sandbox
+        { init = Nothing
+        , view = view
+        , update = always
+        }
+
+
+view : Model -> Svg Msg
+view model =
+    (case model of
+        Just focused ->
+            let
+                base : { circleBg : Bool, clothes : Clothes, skinTone : SkinTone, face : Face, top : Top }
+                base =
+                    Avatars.characterToAvatar focused
+            in
+            (base.top :: allTops)
+                |> List.map (\top -> ( Nothing, { base | top = top } ))
+
+        Nothing ->
+            allCharacters
+                |> List.map (\c -> ( Just c, Avatars.characterToAvatar c ))
+    )
         |> List.indexedMap
-            (\i c ->
-                g [ transform [ Translate (modBy 4 i |> toFloat) ((i // 4) |> toFloat) ] ]
+            (\i ( msg, config ) ->
+                g
+                    [ transform
+                        [ Translate
+                            (modBy 4 i |> toFloat)
+                            ((i // 4) |> toFloat)
+                        ]
+                    , onClick msg
+                    , cursor CursorPointer
+                    ]
                     [ Avataaars.view
                         { width = 1
                         , height = 1
                         }
-                        (Avatars.characterToAvatar c)
+                        config
                     ]
             )
         |> svg
             [ viewBox -0.5 -0.5 5 8
             ]
+
+
+allTops : List Top
+allTops =
+    [ topFacialHair
+    , topHatColorAccessory
+    , topAccessoryFacialHair
+    , topHatColorAccessoryFacialHair
+    , topHairColorAccessoryFacialHair
+    ]
+        |> List.concat
+
+
+topFacialHair : List Top
+topFacialHair =
+    [ TopFacialHair Top.Eyepatch FacialHair.Blank ]
+
+
+topHatColorAccessory : List Top
+topHatColorAccessory =
+    List.Extra.lift3
+        (\t color accessory ->
+            TopHatColorAccessory t color accessory
+        )
+        []
+        []
+        []
+
+
+topAccessoryFacialHair : List Top
+topAccessoryFacialHair =
+    []
+
+
+topHatColorAccessoryFacialHair : List Top
+topHatColorAccessoryFacialHair =
+    []
+
+
+topHairColorAccessoryFacialHair : List Top
+topHairColorAccessoryFacialHair =
+    []
